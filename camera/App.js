06e8c2aeb9e1707840571d7f67dea5da11c2fa4e
YhanMassato <yhanashihara@gmail.com>
2024-03-04 22:21:44 -0300

    criação do projeto

---
+++ camera/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { Camera, cameraType} from 'expo-camera';
+import {useState} from 'react';
+import { Entypo } from '@expo/vector-icons'
+
+
+export default function App() {
+  return (
+   
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
